8035d0adac339b34cf48f9de059ac82006b595ff
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-01-25 19:39:53 +0300

    Калькулятор

--- 2-basics/1-hello-world/task.js
+++ 2-basics/1-hello-world/task.js
@@ -1,5 +1,12 @@
-    // alert( 'Привет, Мир!' );
-var admin;
-var name = 'Василий';
-admin = name;
-alert(admin);
+var num = [];
+while (true) {
+  var value = prompt('Введите число', 0);
+  if (value == '' || value === null || isNaN(value)) break;
+  num.push(+value);
+}
+
+var sum = 0;
+for (var i = 0; i < num.length; i++) {
+  sum += num[i];
+}
+ alert(sum);

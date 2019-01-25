da1f7e27e017f96fbeaef1a076664789f3addb55
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-01-25 20:03:36 +0300

    Поиск в массиве

--- nodelearn.js
+++ nodelearn.js
@@ -1,5 +1,16 @@
-var arr = ['Генеральная уборка', 'Посуда', 'Сырники', 'Колесо', 'Книга', 'Ванна', 'Море', 'Кофе', 'Сон', 'JS', 'css', 'figma', 'Работа', 'Постирать', '4-20'];
+var arr = [1, 2, 3, 4, 5, 6, 'tic', 'tac', 'fuck'];
 
-var rand = Math.floor(Math.random() * arr.length);
+if ([].indexOf) {
+  var find = function(arr, value) {
+    return arr.indexOf(value);
+  }
+} else {
+  var find = function(arr, value) {
+    for (var i = 0; i < arr.length; i++) {
+      if (value === arr[i]) return i;
+    }
+    return -1;
+  }
+}
 
-console.log(arr[rand])
+console.log(find(arr, 4))

d3362b2b4d34903aa152af05e2de6c0f792d09d8
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-01-29 21:45:24 +0300

    Добавить класс в строку

--- nodelearn.js
+++ nodelearn.js
@@ -1,31 +1,18 @@
-var arr = [];
+var obj = {
+  className: 'open menu'
+};
 
-for (var i = 2; i < 100; i++) {
-  arr[i] = true;
+function addClass(obj, cls) {
+  var classes = obj.className ? obj.className.split(' ') : [];
+
+  for (var i = 0; i < classes.length; i++) {
+    if (classes[i] == cls) return;
+  }
+  classes.push(cls);
+  obj.className = classes.join(' ');
 }
 
-var p = 2;
-
-do {
-
-  for (i = 2 * p; i < 100; i += p) {
-    arr[i] = false;
-  }
-
-  for (i = p + 1; i < 100; i++) {
-    if (arr[i]) break;
-  }
-
-  p = i;
-
-} while (p * p < 100);
-
-var sum = 0;
-for (i = 0; i < arr.length; i++) {
-  if (arr[i]) {
-    sum += i;
-  }
-}
-
-
-console.log(sum);
+addClass(obj, 'new');
+addClass(obj, 'open');
+addClass(obj, 'me');
+console.log(obj.className);

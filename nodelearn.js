3fc873e62e2bcb113957ba5fdc6b5efb55ff5b16
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-02-07 23:18:57 +0300

    Массив частичных сумм

--- nodelearn.js
+++ nodelearn.js
@@ -1,7 +1,18 @@
-var arr = ['I', 'shall', 'not', 'live', 'in', 'vain'];
+var arr = [1, 2, 3, 4, 5]
 
-var arrLength = arr.map(function(arrs) {
-  return arrs.length;
-});
+function getSums(arr) {
 
-console.log(arrLength)
+  var result = [];
+  if(!arr.length) return result;
+
+  var totalSum = arr.reduce(function(sum, item) {
+    result.push(sum);
+    return sum + item;
+  });
+
+  result.push(totalSum);
+
+  return result;
+}
+
+console.log(getSums(arr))

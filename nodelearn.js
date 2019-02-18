79f3771d952b049f5ead69b146429a747aba043d
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-02-18 12:36:58 +0300

    Счетчик (замыкания)

--- nodelearn.js
+++ nodelearn.js
@@ -1,35 +1,24 @@
-function formatDate(date) {
-  var diff = new Date() - date;
+function makeCounter() {
+  var currentCount = 1;
 
-  if (diff < 1000) {
-    return "только что";
-  }
+  return {
+    // Получить следующее значение
+    getNext: function() {
+      return currentCount++;
+    },
 
-  var sec = Math.floor(diff / 1000);
+    // Поставить значение
+    set: function(value) {
+      currentCount = value;
+    },
 
-  if (sec < 60) {
-    return sec + " сек. назад";
-
-    var min = Math.floor(diff / 60000);
-    if (min < 60) {
-      return min + " мин. назад";
+    // Обнулить счетчик
+    reset: function() {
+      currentCount = 1;
     }
-
-    var d = date;
-    d = [
-      "0" + d.getDate(),
-      "0" + (d.getMonth() + 1),
-      "" + d.getFullYear(),
-      "0" + d.getHours(),
-      "0" + nd.getMinutes()
-    ];
-
-    for (var i = 0; i < d.length; i++) {
-      d[i] = d[i].slice(-2);
-    }
-    return d.slice(0, 3).join(".") + " " + d.slice(3).join(":");
-  }
+  };
 }
 
-var date = new Date();
-console.log(formatDate(new Date(new Date - 86400 * 1000)));
+var counter = makeCounter();
+
+console.log(counter.getNext());

652ebce23bd10766e4ac6752fc4a6bf7f17ed3c1
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-02-21 22:53:22 +0300

    Добавить get/set свойства

--- nodelearn.js
+++ nodelearn.js
@@ -1,44 +1,31 @@
-function Calculator() {
-  var methods = {
-    "-": function(a, b) {
-      return a - b;
+function User(fullName) {
+  this.fullName = fullName;
+
+  Object.defineProperties(this, {
+    firstName: {
+      get: function() {
+        return this.fullName.split(" ")[0];
+      },
+      set: function(newFirstName) {
+        this.fullName = newFirstName + " " + this.lastName;
+      }
     },
-    "+": function(a, b) {
-      return a + b;
+
+    lastName: {
+      get: function() {
+        return this.fullName.split(" ")[1];
+      },
+      set: function(newLastName) {
+        this.fullName = this.firstName + " " + newLastName;
+      }
     }
-  };
-
-  this.calculate = function(str) {
-    var split = str.split(" "),
-      a = +split[0],
-      op = split[1],
-      b = +split[2]
-
-    if (!methods[op] || isNaN(a) || isNaN(b)) {
-      return NaN;
-    }
-
-    return methods[op](a, b);
-  };
-
-  this.addMethod = function(name, func) {
-    methods[name] = func;
-  };
+  });
 }
 
-var calc = new Calculator;
+var vasya = new User("Василий Пупкин");
 
-calc.addMethod("*", function(a, b) {
-  return a * b;
-});
+console.log(vasya.firstName);
+console.log(vasya.lastName);
 
-calc.addMethod("/", function(a, b) {
-  return a / b;
-});
-
-calc.addMethod("**", function(a, b) {
-  return Math.pow(a, b);
-});
-
-var result = calc.calculate("2 ** 3");
-console.log(result);
+vasya.lastName = "Сидоров";
+console.log(vasya.fullName);

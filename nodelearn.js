84abbfa5ee0a5d8f48970b732a6930f99eb755ae
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-02-23 21:34:53 +0300

    Применение функции к аргументам

--- nodelearn.js
+++ nodelearn.js
@@ -1,7 +1,23 @@
-function sumArg() {
+function sum() {
   return [].reduce.call(arguments, function(a, b) {
     return a + b;
   });
 }
 
-console.log(sumArg(4, 5, 6));
+function mul() {
+  return [].reduce.call(arguments, function(a, b) {
+    return a * b;
+  });
+}
+
+function applyAll(func) {
+  return func.apply(this, [].slice.call(arguments, 1));
+}
+
+console.log(applyAll(sum, 1, 2, 3));
+
+console.log(applyAll(mul, 2, 3, 4));
+
+console.log(applyAll(Math.max, 2, -2, 3));
+
+console.log(applyAll(Math.min, 2, -2, 3));

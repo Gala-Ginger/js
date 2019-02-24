acfc5c597755a0e3d4ffff6fca7dea7ae6414513
Galina Znakharchuk <gala.znaharchuk@gmail.com>
2019-02-24 21:53:57 +0300

    JSON

--- nodelearn.js
+++ nodelearn.js
@@ -1,25 +1,7 @@
-function f(x) {
-  return Math.random() * x;
-}
+var leader = {
+  name: "Василий Иванович",
+  age: 35
+};
 
-function makeCaching(f) {
-  var cach = {};
-
-  return function(x) {
-    if (!(x in cach)) {
-      cach[x] = f.call(this, x);
-    }
-    return cach[x];
-  };
-}
-
-f = makeCaching(f);
-
-var a, b;
-
-a = f(1);
-b = f(1);
-console.log(a == b);
-
-b = f(2);
-console.log(a == b);
+var leaderStr = JSON.stringify(leader);
+leader = JSON.parse(leaderStr);
